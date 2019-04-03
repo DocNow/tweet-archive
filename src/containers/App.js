b136f4f6bfcedfa9a34a89f117881414d8a418a0
raffazizzi <raffaeleviglianti@gmail.com>
2019-04-03 17:18:45 -0400

    added randomize function with shake

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import TweetViewer from 'tweet-viewer'
 import Metadata from '../components/Metadata'
 import Range from 'rc-slider/lib/Range';
+import Shake from 'shake.js'
 import 'rc-slider/assets/index.css';
 import './App.css'
 
@@ -16,14 +17,27 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({
+      tweetIds: this.props.tweetIds
+    })
+    this.shakeEvent = new Shake({
+      threshold: 15,
+      timeout: 1000
+    })
+    this.shakeEvent.start()
+    window.addEventListener('shake', () => {
+      this.shuffle()
+    }, false)
+  }
+
   reduceIds(steps) {
-    console.log(steps)
-    const tot = this.props.tweetIds.length 
+    const tot = this.state.tweetIds.length 
     const tweetsPerStep = Math.floor(tot / steps)
     const remainder = tot - (tweetsPerStep * steps)
     const firstStep = this.state.firstStep
     const lastStep = this.state.lastStep === Infinity ? steps : this.state.lastStep
-    return this.props.tweetIds.reduce((acc, id, idx) => {
+    return this.state.tweetIds.reduce((acc, id, idx) => {
       if (idx >= tweetsPerStep * firstStep && idx < tweetsPerStep * lastStep) {
         acc.push(id)
       }
@@ -33,6 +47,13 @@ class App extends React.Component {
       }
       return acc
     }, [])
+  }
+
+  resetIds() {
+    this.setState({
+      firstStep: 0,
+      lastStep: Infinity
+    })
   }
 
   onSlide(values) {
@@ -46,22 +67,41 @@ class App extends React.Component {
     }
   }
 
+  shuffle() {
+    const tis = Array.from(this.state.tweetIds)
+    for (let i = tis.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [tis[i], tis[j]] = [tis[j], tis[i]];
+    }
+    this.setState({
+      tweetIds: tis
+    })
+    this.resetIds()
+  }
+
   render() {
+    if (!this.state.tweetIds) {
+      return null
+    }
     // Bring range of tweets to a more manageable number
-    let maxRange = this.props.tweetIds.length
+    let maxRange = this.state.tweetIds.length
     while (maxRange > 100) {
       maxRange = Math.floor(maxRange / 100)
     }
-    // this.props.tweetIds.length / maxRange
     const tweetIds = this.reduceIds(maxRange)
-    console.log(tweetIds.length, this.props.tweetIds.length)
     return(
       <div id="App">
         <Metadata metadata={this.props.metadata} />
-        <div>
-          <span className="Label">Limit number of tweets</span>
-          <Range allowCross={false} min={0} max={maxRange} defaultValue={[0, maxRange]} onChange={(r) => this.onSlide(r)}/>
-        </div>
+        <div className="Operations">
+          <div>
+            <span className="OpsLabel">Shuffle (click or shake!) </span>
+            <button onClick={() => {this.shuffle()}}>🔀</button>
+          </div>
+          <div>
+            <span className="OpsLabel">Limit number of tweets</span>
+            <Range allowCross={false} min={0} max={maxRange} defaultValue={[0, maxRange]} onChange={(r) => this.onSlide(r)}/>
+          </div>
+          </div>
         <TweetViewer tweetIds={tweetIds} />
       </div>
     )
